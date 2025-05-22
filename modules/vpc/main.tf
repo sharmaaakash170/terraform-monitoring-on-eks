44450e41dcadcc314e25da5c7036b2bb1dc19885
sharmaaakash170 <sharmaaakash170@gmail.com>
2025-05-22 12:10:39 +0530

    Updated infra.yaml file, now it will check or create the dynamodb table

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -56,18 +56,17 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_route_table" "public_rt" {
-  count = length(var.public_subnet_cidrs)
   vpc_id = aws_vpc.this.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw[count.index].id
+    gateway_id = aws_internet_gateway.igw.id
   }
 }
 
 resource "aws_route_table_association" "this" {
   count          = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public_rt[count.index].id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_nat_gateway" "nat_gw" {
